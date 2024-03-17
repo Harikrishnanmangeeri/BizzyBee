49a902165aebda9fe369ebc3685d79cb3c0dba7e
Harikrishnanmangeeri <hkmangeeri@gmail.com>
2024-03-17 22:12:54 +0530

    try

--- bizzybee/src/components/landing.tsx
+++ bizzybee/src/components/landing.tsx
@@ -1,8 +1,37 @@
-"use client";
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
 import { SparklesCore } from "./ui/sparkles";
+import axios, { Axios } from "axios";
 
 export function Landing() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae");
+        const data = await res.json();
+        // console.log(data); 
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae');
+        const data = response.data;
+        // console.log(data); 
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+ 
   return (
     <div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <div className="w-full absolute inset-0 h-screen">
--- bizzybeeBackend/Controller/user.controller.js
+++ bizzybeeBackend/Controller/user.controller.js
@@ -1,12 +1,13 @@
 const userSchema = require("../Modal/user");
 const { joiuserschema } = require("../Modal/validationschema");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   registration: async (req, res) => {
     const { value, error } = joiuserschema.validate(req.body);
     const { username, password, email, contact } = value;
-    console.log(req.body)
+    console.log(req.body);
     if (error) {
       res.json(error.message);
     } else {
@@ -24,11 +25,62 @@ module.exports = {
         res.json("This registration add to database sucessfully");
       }
     }
-    
   },
   login: async (req, res) => {
-    const { contact , email , password} = req.body;
-    const user = await userschema.find({})
+    const { value, error } = joiuserschema.validate(req.body);
+    const { email, password } = value;
+
+    if (error) {
+      return res.json(error.message);
+    }
+
+    try {
+      const user = await userSchema.findOne({ email: email });
+
+      if (!user) {
+        return res.send("User unavailable");
+      }
+
+      const checkpass = await bcrypt.compare(password, user.password);
+
+      if (!checkpass) {
+        return res.json("Password incorrect");
+      }
+
+      const resp = {
+        id: user.id,
+      };
+
+      const token = jwt.sign(resp, process.env.ACCESS_USERTOKEN_SECRET, {
+        expiresIn: 86400,
+      });
+
+      res.send({
+        status: "success",
+        auth: true,
+        token: token,
+        isBlocked: user.isBlocked,
+        id: user.id,
+      });
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
-  profile: async (req, res) => {},
+
+  profile: async (req, res) => {
+    try {
+      const profile = await userSchema.findOne({ _id: res.token });
+  
+      if (profile) {
+        res.json(profile);
+      } else {
+        res.status(404).json("User not found!");
+      }
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+  
 };
