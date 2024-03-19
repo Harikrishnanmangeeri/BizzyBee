23d2b7c3b67ea96f57165eb938a7afde0ac8d517
Harikrishnanmangeeri <hkmangeeri@gmail.com>
2024-03-19 21:02:19 +0530

    /

--- bizzybee/src/components/landing.tsx
+++ bizzybee/src/components/landing.tsx
@@ -18,19 +18,19 @@ export function Landing() {
     fetchData();
   }, []);
   
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae');
-        const data = response.data;
-        // console.log(data); 
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae');
+  //       const data = response.data;
+  //       // console.log(data); 
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
  
   return (
     <div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
